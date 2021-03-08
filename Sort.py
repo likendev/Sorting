6b6d035c85eb56ac3e4b599d443b14a48dbf5c47
liken <li.ken@ionnex.com>
2021-03-08 17:32:07 +0800

    Bubble sort, selection sort

--- Sort.py
+++ Sort.py
@@ -1,2 +1,22 @@
+def bubble_sort(nums: list[int]) -> list[int]:
+    for i in range(len(nums)):
+        for j in range(len(nums) - 1):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+    return nums
+
+
+def selection_sort(nums: list[int]) -> list[int]:
+    for i in range(len(nums)):
+        min_index = i
+        for j in range(i + 1, len(nums)):
+            if nums[j] < nums[i]:
+                min_index = j
+        nums[i], nums[min_index] = nums[min_index], nums[i]
+    return nums
+
+
 if __name__ == '__main__':
-    print("Hello world!")
+    nums = [45, 55, 35, 25]
+    print('Bubble Sort: ' + ', '.join(str(e) for e in bubble_sort(nums)))
+    print('Selection Sort: ' + ', '.join(str(e) for e in selection_sort(nums)))
